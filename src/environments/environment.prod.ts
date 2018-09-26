eb260556d8bc0a7c8a99fadb914bdaf17212af47
akanshmahendra <akki619@icloud.com>
2018-09-26 10:28:00 +0530

    Push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyD6RDNKeIAf4xHZcoEVcdqIS21YzZJt2QA",
+    authDomain: "client-pannel-prod-577bd.firebaseapp.com",
+    databaseURL: "https://client-pannel-prod-577bd.firebaseio.com",
+    projectId: "client-pannel-prod-577bd",
+    storageBucket: "client-pannel-prod-577bd.appspot.com",
+    messagingSenderId: "47323527835"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyD6RDNKeIAf4xHZcoEVcdqIS21YzZJt2QA",
+    authDomain: "client-pannel-prod-577bd.firebaseapp.com",
+    databaseURL: "https://client-pannel-prod-577bd.firebaseio.com",
+    projectId: "client-pannel-prod-577bd",
+    storageBucket: "client-pannel-prod-577bd.appspot.com",
+    messagingSenderId: "47323527835"
+  }
 };
